7613f0c6ada76014b3f7639c18240d4a49cb9ea5
pongsil vachirajongkol <pongsil@set.or.th>
2018-11-12 23:20:30 +0700

    change title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,9 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to Waan-Hymn Wedding
-          </p>
+          <p>Welcome to Waan-Hymn Wedding</p>
           <a
             className="App-link"
             href="https://goo.gl/maps/CJCDcZQZXVA2"
