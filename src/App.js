cda186f4c193c1de56db2fd502404a7c4577f1fb
pongsil vachirajongkol <pongsil@set.or.th>
2018-11-13 01:20:29 +0700

    html template

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Welcome to Waan-Hymn Wedding</p>
-          <a
-            className="App-link"
-            href="https://goo.gl/maps/CJCDcZQZXVA2"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Click here
-          </a>
-        </header>
+        xxx
       </div>
     );
   }
